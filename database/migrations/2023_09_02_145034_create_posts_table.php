96b0dcc21692d81f8fbe5b323cbfdef701f09e19
ghongatevivek <ghongatevivek1212@gmail.com>
2023-09-16 17:48:35 +0530

    Solved migration syntax issue

--- database/migrations/2023_09_02_145034_create_posts_table.php
+++ database/migrations/2023_09_02_145034_create_posts_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->tinyInteger('status',1)->default(1);
+            $table->tinyInteger('status')->default(1);
             $table->text('description')->nullable();
             $table->timestamps();
         });
